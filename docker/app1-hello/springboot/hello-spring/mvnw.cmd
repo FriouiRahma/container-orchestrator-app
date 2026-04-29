8cd6e9238d77bed45d37889350c3b5bd29d6b498
rfrioui <rfrioui@elyosdigital.com>
2026-04-29 11:42:53 +0200

    modification des fichiers yaml

--- docker/app1-hello/springboot/hello-spring/mvnw.cmd
+++ docker/app1-hello/springboot/hello-spring/mvnw.cmd
@@ -89,10 +89,16 @@ if (-not (Test-Path -Path $MAVEN_M2_PATH)) {
 }
 
 $MAVEN_WRAPPER_DISTS = $null
-if ((Get-Item $MAVEN_M2_PATH).Target[0] -eq $null) {
-  $MAVEN_WRAPPER_DISTS = "$MAVEN_M2_PATH/wrapper/dists"
+$mavenUserHomeItem = Get-Item -LiteralPath $MAVEN_M2_PATH
+$target = $null
+if ($mavenUserHomeItem.PSObject.Properties.Match('Target').Count -gt 0) {
+  $target = $mavenUserHomeItem.Target
+}
+if ($target) {
+  $target0 = if ($target -is [System.Array]) { $target[0] } else { $target }
+  $MAVEN_WRAPPER_DISTS = "$target0/wrapper/dists"
 } else {
-  $MAVEN_WRAPPER_DISTS = (Get-Item $MAVEN_M2_PATH).Target[0] + "/wrapper/dists"
+  $MAVEN_WRAPPER_DISTS = "$MAVEN_M2_PATH/wrapper/dists"
 }
 
 $MAVEN_HOME_PARENT = "$MAVEN_WRAPPER_DISTS/$distributionUrlNameMain"
